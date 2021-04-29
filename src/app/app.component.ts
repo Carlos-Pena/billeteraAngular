229144ec8dd7c90a6cd29b88fc2b27b2f5e07839
Carlos Peña <carlosaph28@gmail.com>
2021-04-29 16:03:49 -0500

    cabecera y formulario css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'billeteraAngular';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.presupuestoDisponible = this.ingresos - this.egresos
+    }
+  title = 'Presupuesto Disponible:';
+
+  presupuestoDisponible : number = 1000;
+  ingresos : number = 4500;
+  egresos : number = 1000;
 }
